8952125f2e0cb4347e40aec42b99239a69c7397d
James <James@192.168.0.14>
2019-04-19 12:46:02 -0400

    wtf am i doing

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -5,7 +5,9 @@
  */
 package ca.sheridancollege.project;
 
+import ca.sheridancollege.project.Card.Value;
 import java.util.ArrayList;
+import java.util.Scanner; 
 
 /**
  * The class that models your game. You should create a more specific
@@ -14,18 +16,40 @@ import java.util.ArrayList;
  */
 
 
-public class Game 
-{
-    private String gameName;//the title of the game
-    private ArrayList <Player> _players;// the players of the game
+public class Game {
+ 
     
    public static void main(String[] args) {
+       
+    Scanner console= new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter player name:");
+
+    String userName = console.nextLine(); // Read user input
+      
+    Player _player = new Player(userName, 100);
+    Dealer _dealer = new Dealer("Dealer", 100);
+    
+    ShuffleDeal dealing = new ShuffleDeal();
+    
+    dealing.deal(_player);
+    dealing.deal(_dealer);
 
 }
     
- 
+    public int handValue(Hand _hand){
 
+   int value = 0;
+   
+   for(int i = 0; i < _hand.showCards().size(); i++) {
+       
+       Value tmp = _hand.showCards().get(i).getValue();
+       
+       if (tmp == Value.TWO)
+   }
     
+
+}
+
     public void declareWinner(){;
 
    
