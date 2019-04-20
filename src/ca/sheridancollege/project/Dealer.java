95f81456aa7ce86a21e79f9827541d5b603a913a
ArseniiTi <tiurin@sheridancollege.ca>
2019-04-19 20:20:05 -0400

    Comments for methods and error fix

--- src/ca/sheridancollege/project/Dealer.java
+++ src/ca/sheridancollege/project/Dealer.java
@@ -1,10 +1,13 @@
 package ca.sheridancollege.project;
 
+/**
+ * This class creates a dealer who will play with player
+ */
 public class Dealer extends Player {
 
     public Dealer(String name, double money) {
         super(name, money);
     }
 
-    
+
 }
